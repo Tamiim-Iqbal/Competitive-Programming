3311315fe0b546036f3988b8d1762a9eecb03074
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-03-17 16:34:38 +0600

    800 : Cover in Water

--- TLE-Eliminators/800/A_Cover_in_Water.cpp
+++ TLE-Eliminators/800/A_Cover_in_Water.cpp
@@ -1,3 +1,8 @@
+/**
+ *   author:  TAMIM IQBAL
+ *   created: 17/03/2025 16:29:00
+**/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -14,10 +19,8 @@ int main()
 
         string s, search = "..."; 
         cin >> s;
- 
-        size_t found = s.find(search);
         
-        if(found != string::npos)
+        if(s.find(search) != -1)
         {
            cout<<2<<endl;
         }
@@ -29,7 +32,26 @@ int main()
             }
             cout<<count<<endl;
         }
-
     }
     return 0;
 }
+
+
+
+// In C++, string::npos is a special constant that represents "no position" or an invalid index in a string.
+//You can replace string::npos with -1 since string::npos is typically defined as -1 (or the maximum value of size_t
+
+
+//         size_t found = s.find(search);   
+//         if(found != string::npos) 
+//         {
+//            cout<<2<<endl;
+//         }
+//         else
+//         {
+//             for(char c : s)
+//             {
+//                 if(c == '.') count++;
+//             }
+//             cout<<count<<endl;
+//         }
