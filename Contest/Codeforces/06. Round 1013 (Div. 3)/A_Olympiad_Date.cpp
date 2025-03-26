7617bd7b2efb493fe3b26ad7f7179afb7b4bbea2
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-03-26 17:10:24 +0600

    Olympiad Date [1013 : Div3 : A]

---
+++ Contest/Codeforces/06. Round 1013 (Div. 3)/A_Olympiad_Date.cpp
@@ -0,0 +1,40 @@
+
+// Time Complexity : O(t * n)
+// Space Complexity : O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n; cin >> n; 
+
+    int freq[10] = {};
+    bool flag = false;
+
+    for(int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        freq[x]++;
+        if(!flag && freq[0] >= 3 && freq[1] >= 1 && freq[2] >= 2 && freq[3] >= 1 && freq[5] >= 1)
+        {
+            cout << i + 1 << "\n";
+            flag = true;
+        }
+    }
+
+    if(!flag) cout << 0 << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t; cin >> t;
+    while(t--)
+    {
+        solve();   
+    }
+    return 0;
+}
