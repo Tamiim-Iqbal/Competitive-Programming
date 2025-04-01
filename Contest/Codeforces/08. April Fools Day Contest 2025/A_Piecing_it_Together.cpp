40fa4a5e091cf46c83d686cf8e7e263fbd85c930
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-04-01 21:19:21 +0600

    Piecing it Together [April Fools Contest 2025 : A]

---
+++ Contest/Codeforces/08. April Fools Day Contest 2025/A_Piecing_it_Together.cpp
@@ -0,0 +1,15 @@
+/**
+ *   author:  tamim-iqbal
+ *   created: 01/04/2025 20:37:14
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << "puzzling" << endl;
+    return 0;
+}
