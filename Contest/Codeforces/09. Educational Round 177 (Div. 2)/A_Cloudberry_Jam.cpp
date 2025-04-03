23cec316609edcba76a1dc25d2f0bbc5d3b0a936
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-04-03 22:35:21 +0600

    Cloudberry Jam [E 177 : Div2 : A]

---
+++ Contest/Codeforces/09. Educational Round 177 (Div. 2)/A_Cloudberry_Jam.cpp
@@ -0,0 +1,35 @@
+/**
+ *   author:  tamim-iqbal
+ *   created: 03/04/2025 20:37:01
+**/
+
+// Time Complexity : 
+// Space Complexity :
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+#define endl "\n";
+#define ff first
+#define ss second
+
+void solve()
+{
+    int n; cin >> n;
+    cout << n * 2 << endl;
+
+}
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
