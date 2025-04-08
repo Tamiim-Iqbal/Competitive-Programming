553925a54fcbc4e9a70a397ee8b8938c2ff22b98
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-04-09 00:55:29 +0600

    Simple Repetition [1016 : Div3 : C]

---
+++ Contest/Codeforces/11. Round 1015 (Div 3)/C_Simple_Repetition.cpp
@@ -0,0 +1,63 @@
+// Time Complexity : sqrt(n)
+// Space Complexity : O(1)
+
+#define _Alignof alignof       // ignore this : its just for the compiler
+#include<bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define Dpos(n) fixed << setprecision(n)
+#define endl "\n";
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define f(i, start, end) for(int i = start; i < end; i++)
+#define fe(i, end, e) for(int i = end - 1; i >= e; i--)
+#define fl(i, start, end) for(ll i = start; i < end; i++)
+
+
+bool isPrime(ll n)
+{
+    f(i, 2, sqrt(n))
+    {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    ll x, k; cin >> x >> k;
+    
+    if (x == 1 && k == 2) yes 
+    else if (x == 1 || k > 1) no
+    else 
+    {
+        if(isPrime(x)) yes else no
+    }
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);  cin.tie(NULL);
+
+    int t;  cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
