8d80f2024f91713e2ac6d2f56317c653d18e11ae
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-04-11 01:25:00 +0600

    800 : Twin Permutations

---
+++ TLE-Eliminators/800/A_Twin_Permutations.cpp
@@ -0,0 +1,50 @@
+// Time Complexity : 
+// Space Complexity :
+
+#define _Alignof alignof       // ignore this : its just for the compiler
+#include<bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define Dpos(n) fixed << setprecision(n)
+#define endl "\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve()
+{
+    int n; cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);  cin.tie(NULL);
+
+    int t;  cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
