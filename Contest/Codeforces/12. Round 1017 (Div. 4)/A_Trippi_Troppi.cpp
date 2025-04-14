ba84dac6595e19eb1d73715bf1181b81c2e9ae14
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-04-14 22:57:45 +0600

    Trippi Troppi [1017 : Div 4 : A]

---
+++ Contest/Codeforces/12. Round 1017 (Div. 4)/A_Trippi_Troppi.cpp
@@ -0,0 +1,48 @@
+/**
+ *   author:  tamim-iqbal
+ *   created: 13/04/2025 21:37:29
+**/
+
+// Time Complexity : 
+// Space Complexity : 
+
+#define _Alignof alignof       // ignore this : its just for the compiler
+#include<bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define Dpos(n) fixed << setprecision(n)
+#define endl "\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve()
+{
+    string a, b, c; cin >> a >> b >> c;
+    cout << a[0]<< b[0]<< c[0] << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);  cin.tie(NULL);
+
+    int t;  cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
