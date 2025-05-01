15d4b2b091f4ab1ba335f911fa1fce7ceab6bb90
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-05-01 21:07:29 +0600

    1022 : Div2 : A

---
+++ Contest/Codeforces/14. Round 1022 (Div. 2)/A_Permutation_Warm_Up.cpp
@@ -0,0 +1,50 @@
+/**
+ *   author:  tamim-iqbal
+ *   created: 01/05/2025 21:01:40
+**/
+
+#define _Alignof alignof       // ignore this : its just for the compiler
+#include<bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define Dpos(n) fixed << setprecision(n)
+#define endl "\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve()
+{
+    int n; cin >> n;
+    if(n == 1) cout << 1 << endl;
+    else
+    {
+        int ans = n * n / 4 + 1;
+        cout << ans << endl;
+    }
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);  cin.tie(NULL);
+
+    int t;  cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
